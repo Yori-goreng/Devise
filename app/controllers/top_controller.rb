fb54bc3c8d9ced29961d7b3e6eb9240b746dea7d
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-05-15 00:09:44 +0900

    リスト作成機能の実装

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,5 @@
-class ItemsController < ApplicationController
-    def index
+class TopController < ApplicationController
+    def index 
         @lists = List.where(user: current_user).order("created_at ASC")
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'items#index'
+  root to: 'top#index'
+  
+  resources :list, only: %i(new create)
   
 end
