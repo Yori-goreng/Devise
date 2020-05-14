f0129637a3a157714800b02c2e93a70044f959d3
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-05-14 16:52:39 +0900

    ヘッダーにユーザーネームを表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
     def show
         @user = User.find(current_user.id)
     end
+
+    def after_sign_up_path_for
+        root_path
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_08_121533) do
-
-  create_table "items", force: :cascade do |t|
-    t.string "name"
-    t.integer "price"
-    t.integer "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 2020_05_11_042030) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_05_08_121533) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
