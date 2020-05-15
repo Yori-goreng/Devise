cedf0691c7b3b4835d4c8ceaabe780e03cc81993
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-05-15 13:21:39 +0900

    カード編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users
   
   resources :list, only: %i(new create edit update destroy) do
-    resources :card, only: %i(new create)
+    resources :card, only: %i(new create show)
   end
-  
+
 end
