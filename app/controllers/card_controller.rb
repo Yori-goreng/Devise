f6476191d0455c739479d06e02145190144fab0e
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-05-18 17:07:01 +0900

    test

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -2,9 +2,10 @@ class CardController < ApplicationController
     before_action :set_card, only: %i(show edit update destroy)
   
     def new
-      @card = Card.new
+      @card = Card.new      
       @list = List.find_by(id: params[:list_id])
     end
+
     def create
       @card = Card.new(card_params)
       if @card.save
@@ -33,10 +34,14 @@ class CardController < ApplicationController
       @card.destroy
       redirect_to :root
     end
-  
+
     private
+
+    def card_params
+      params.require(:card).permit(:title, :memo, :list)
+    end
      
-      def set_card
-        @card = Card.find_by(id: params[:id])
-      end
+    def set_card
+      @card = Card.find_by(id: params[:id])
+    end
   end
